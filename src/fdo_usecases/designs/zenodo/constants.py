ee10fc640eb59cba86caf4224e14ef5813216e48
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2026-08-21 13:35:28 +0200

    minor fies
    Signed-off-by: Maximilian Inckmann <maximilian.inckmann@kit.edu>

--- src/fdo_usecases/designs/zenodo/constants.py
+++ src/fdo_usecases/designs/zenodo/constants.py
@@ -39,61 +39,61 @@ PUBLICATION_PROFILE = "21.T11969/e00441c49bf6cb62a4a5"
 # =============================================================================
 
 INFOTYPES = {
+    #: Cryptographic checksum
+    "checksum": "21.T11969/a80ed2ef79e22f1d8af8",
+    #: Publication cites relationship
+    "cites": "21.T11969/813e06c1441327b72e68",
     #: Creator ORCID identifier
     "creator": "21.T11969/7c67083a5d218e544063",
     #: Creator institutional affiliation (ROR ID)
     "creatorAffiliation": "21.T11969/ea9f6b3d78c6608fe801",
+    #: DataCite publication type
+    "dataCitePublicationType": "21.T11969/48dbf6a89f9748ae4ead",
+    #: Download URL for data object
+    "dataObjectLocation": "21.T11969/479febb2bbe8400da547",
     #: Resource creation date (ISO 8601)
     "dateCreated": "21.T11969/29f92bd203dd3eaa5a1f",
     #: Resource modification date (ISO 8601)
     "dateModified": "21.T11969/397d831aa3a9d18eb52c",
-    #: Subject keywords
-    "keyword": "21.T11969/793ff5c33c3aeb32907a",
-    #: Resource name/title
-    "name": "21.T11969/bd3e9fb9b606d2198c9e",
+    #: Publication date (ISO 8601)
+    "datePublished": "21.T11969/0c9b86e828976a85d4f2",
     #: Resource description/abstract
     "description": "21.T11969/880724416f5857987e70",
+    #: Digital Object Identifier
+    "doi": "21.T11969/48e563f148dc04d8b31c",
+    #: Dataset hasData relationship (linking to files)
+    "hasData": "21.T11969/cc230f978e8add2e2520",
+    "hasPart": "21.T11969/39c056d0cc63e2d0ff17",
+    #: Publication isCitedBy relationship
+    "isCitedBy": "21.T11969/520bb71b795d8573f533",
+    #: File isPartOf relationship (linking to datasets)
+    "isPartOf": "21.T11969/30f4a1f8aacab81faf38",
+    #: Publication isReferencedBy relationship
+    "isReferencedBy": "21.T11969/e5945ef3ff07f314a146",
+    #: Subject keywords
+    "keyword": "21.T11969/793ff5c33c3aeb32907a",
+    #: Landing page URL location
+    "landingPageLocation": "21.T11969/8710d753ad10f371189b",
+    #: Latest version DOI/Handle
+    "latestVersion": "21.T11969/2b4d6ceda80ddd63f7a9",
     #: Media/MIME type
     "mimeType": "21.T11969/3313b863118ed5eb0ded",
-    #: Download URL for data object
-    "dataObjectLocation": "21.T11969/479febb2bbe8400da547",
-    #: SPDX license URL
-    "spdxLicense": "21.T11969/623654b1072ae7b88202",
-    #: Cryptographic checksum
-    "checksum": "21.T11969/a80ed2ef79e22f1d8af8",
-    #: Semantic version string
-    "version": "21.T11969/be1ae3492b235faad933",
+    #: Resource name/title
+    "name": "21.T11969/bd3e9fb9b606d2198c9e",
     #: Next version DOI/Handle
     "nextVersion": "21.T11969/7f1a6afddcfeefbf195b",
     #: Previous version DOI/Handle
     "previousVersion": "21.T11969/7c97f00a2a95826c1a8f",
-    #: Latest version DOI/Handle
-    "latestVersion": "21.T11969/2b4d6ceda80ddd63f7a9",
-    #: Digital Object Identifier
-    "doi": "21.T11969/48e563f148dc04d8b31c",
-    #: DataCite publication type
-    "dataCitePublicationType": "21.T11969/48dbf6a89f9748ae4ead",
-    #: Publisher name
-    "publisher": "21.T11969/cdd96207a7dfbcc0db93",
-    #: Publication date (ISO 8601)
-    "datePublished": "21.T11969/0c9b86e828976a85d4f2",
-    #: Dataset hasData relationship (linking to files)
-    "hasPart": "21.T11969/39c056d0cc63e2d0ff17",
-    "hasData": "21.T11969/cc230f978e8add2e2520",
-    #: File isPartOf relationship (linking to datasets)
-    "isPartOf": "21.T11969/30f4a1f8aacab81faf38",
-    #: Publication cites relationship
-    "cites": "21.T11969/813e06c1441327b72e68",
-    #: Publication isCitedBy relationship
-    "isCitedBy": "21.T11969/520bb71b795d8573f533",
-    #: Publication references relationship
-    "references": "21.T11969/f4c9a69f715c3c60aa2f",
-    #: Publication isReferencedBy relationship
-    "isReferencedBy": "21.T11969/e5945ef3ff07f314a146",
-    #: Landing page URL location
-    "landingPageLocation": "21.T11969/8710d753ad10f371189b",
     #: Preview image URL for dataset landing page
     "previewImage": "21.T11969/925e3f9925a88476ffba",
+    #: Publisher name
+    "publisher": "21.T11969/cdd96207a7dfbcc0db93",
+    #: Publication references relationship
+    "references": "21.T11969/f4c9a69f715c3c60aa2f",
+    #: SPDX license URL
+    "spdxLicense": "21.T11969/623654b1072ae7b88202",
+    #: Semantic version string
+    "version": "21.T11969/be1ae3492b235faad933",
 }
 
 
